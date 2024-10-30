aac4fe19058935117fd5c77e61a98b7738d39a14
WonJaeNo <nodnjs1231@quve.kr>
2024-10-30 22:44:49 +0900

    Modify : Loader SCSS, Toast option

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { app, db } from "firebaseApp";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app, db } from 'firebaseApp';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Router from './components/Router';
-import { useContext, useEffect, useState } from "react";
-import { ToastContainer } from "react-toastify";
+import { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Loader from "components/Loader";
-import { ThemeContext } from "context/ThemeContext";
+import Loader from 'components/loader/Loader';
+import { ThemeContext } from 'context/ThemeContext';
 
 function App() {
   const context = useContext(ThemeContext);
 
   // getAuth 함수는 app을 넣어줘야 동작합니다.
-  const auth = getAuth(app); 
+  const auth = getAuth(app);
 
   // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -21,22 +21,22 @@ function App() {
     !!auth?.currentUser
   );
 
-  useEffect(() =>  {
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if(user){
+      if (user) {
         setIsAuthenticated(true);
-      }else{
+      } else {
         // User is sign out
         setIsAuthenticated(false);
       }
       setInit(true);
     });
-  },[]);
-  
+  }, []);
+
   return (
-    <div className={context.theme === "light" ? "white" : "dark"}>
-      <ToastContainer />
-      {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader />}
+    <div className={context.theme === 'light' ? 'white' : 'dark'}>
+      <ToastContainer autoClose={1000} hideProgressBar newestOnTop />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </div>
   );
 }
